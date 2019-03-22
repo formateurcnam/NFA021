b73ae1774e196231baa788d757346f99f535161a
OARIBEIRO <48176199+OARIBEIRO@users.noreply.github.com>
2019-03-22 14:45:20 +0100

    Add files via upload

--- tchat/authentification/client.php
+++ tchat/authentification/client.php
@@ -1,3 +1,27 @@
+<?php
+  if (isset($_POST['connexion'])) {
+    extract($_POST);
+    try {
+      $bdd = new PDO('mysql:host=localhost;dbname=tchat', 'root', '' );
+      $donnees = $bdd->prepare('SELECT id FROM  client WHERE pseudo = ?');
+      $donnees->execute(array($pseudo));
+      $psd = $donnees->fetch();
+      if (password_verify($mdp, $donnees->mdp)) {
+        session_start();
+        $_SESSION['auth'] = $pseudo;
+        $_SESSION['flash']['success'] = "Vous êtes bien connecté au tchat";
+        header('Location: ../tchat.php');
+    }else {
+       $_SESSION['flash']['danger'] = "pseudo ou mot de passe incorrect";
+     }
+    }
+   catch (PDOException $e) {
+      die('Erreur : ' .$e->getMessage());
+   }
+ }
+
+?>
+
 <h1>Authentification - Client </h1>
 <form action="" method="post">
   <input type="text" name="pseudo" placeholder="Votre pseudo"><br>
